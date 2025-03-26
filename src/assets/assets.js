7b1e6a7dabb731087bf4137daa8d1e0d6dff573b
Apiset Chanchamroon <Apiset1012@hotmail.com>
2025-03-26 22:26:43 +0700

    Update Login and Add Another anime template

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,15 @@ import search_icon from "./search_icon.svg";
 import solo from "./solo.jpg";
 import qwerty from "./qwerty.png";
 import caret_right from './caret-right-solid.svg'
+import rezero from './rezero.png'
+import retext from './retext.png'
 
 export const assets = {
   crunchyroll,
   search_icon,
   solo,
   qwerty,
-  caret_right
+  caret_right,
+  rezero,
+  retext
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+
 
 const Navbar = () => {
+
+  const { openSignIn } = useClerk()
+  const { user } = useUser()
   
   return (
     <div className='flex items-center justify-between'>
@@ -15,7 +20,9 @@ const Navbar = () => {
       <div className='bg-[#454545] rounded-sm border-b-amber-500 focus-within:border-b-2'>
         <input type="text" placeholder='Search ..' className='w-full placeholder-white text-white text-lg py-1 pl-4 focus:pr-72 outline-none transition-all duration-400' />
       </div>
-      <button className='cursor-pointer block hover:-translate-y-0.5 transition-transform text-white px-10 py-5 mr-10'>Create Account</button>
+      <div className='block hover:-translate-y-0.5 transition-transform text-white px-10 py-5 mr-10'>
+        { user ? <UserButton /> : <button onClick={() => openSignIn()} className='cursor-pointer'>Create Account</button>}
+        </div>
     </div>
   )
 }
