665501ed12fd996a21dc050a3c73c3091cf3e929
Apiset Chanchamroon <Apiset1012@hotmail.com>
2025-04-03 23:18:47 +0700

    update on arrow functionality

--- src/components/Allmovies.jsx
+++ src/components/Allmovies.jsx
@@ -39,14 +39,14 @@ const Allmovies = () => {
 
   return (
     <div>
-      <div className="absolute left-2 bottom-32 w-12 cursor-pointer" onClick={handleLeftClick}><img src={assets.back} alt="Back arrow" /></div>
-      <div className="absolute right-2 bottom-32 w-12 cursor-pointer" onClick={handleRightClick}><img src={assets.next} alt="Right arrow" /></div>
-      <div className="container max-w-11/12 mx-auto -mt-[400px] absolute left-20 overflow-hidden ">
+      <div className={`absolute left-2 bottom-32 w-12 cursor-pointer ${currentIndex == 0 ? 'hidden' : ''}`} onClick={handleLeftClick}><img src={assets.back} alt="Back arrow" /></div>
+      <div className={`absolute right-2 bottom-32 w-12 cursor-pointer ${currentIndex == 24 ? 'hidden' : ''}`} onClick={handleRightClick}><img src={assets.next} alt="Right arrow" /></div>
+      <div className="container max-w-11/12 mx-auto -mt-[400px] absolute left-20 overflow-hidden  ">
         <h1 className=" text-3xl font-bold tracking-wide">Anime Watchlist</h1>
         <p className="text-[#9d9d9d]">Unwind this weekend with your top anime picks</p>
-        <div className="w-full text-center whitespace-nowrap">
+        <div className="w-full text-center whitespace-nowrap ">
           {animeList && animeList.slice(currentIndex, currentIndex + 6).map((items) => (
-            <div key={items.mal_id} className="py-4 rounded-sm inline-block space-x-9">
+            <div key={items.mal_id} className="py-4 rounded-sm inline-block space-x-9 transition-all duration-300 ease-in ">
               <div>
                 <img src={items.images.jpg.image_url} alt="image" className="block mx-auto w-[260px] h-[325px] rounded-sm" />
                 </div>
