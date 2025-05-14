9950286cedaaf8e5f70781742d812513703be902
Apiset Chanchamroon <Apiset1012@hotmail.com>
2025-05-14 23:53:08 +0700

    Add anime news feature to Details component: integrate useAnimeNewsApi and display news titles

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,4 +1,5 @@
 import { useAnimeApi } from "@/hooks/useAnimeApi";
+import { useAnimeNewsApi } from "@/hooks/useAnimeNewsApi";
 import { formatSiUnit } from "format-si-unit";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -7,6 +8,9 @@ const Details = () => {
   const { animeList } = useAnimeApi(id as string);
   const [activeTab, setActiveTab] = useState('overview')
   const [showVideo, setShowVideo] = useState(false)
+  const { animeNews } = useAnimeNewsApi('16498')
+  console.log('This is animeNews:', animeNews);
+
 
   const handleChange = () => {
     if (activeTab === 'overview') {
@@ -15,6 +19,8 @@ const Details = () => {
       setShowVideo(false)
     }
   }
+
+
 
   useEffect(() => {
     handleChange()
@@ -184,9 +190,14 @@ const Details = () => {
                   <h2 className="text-2xl font-bold mb-4 flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info mr-2 h-5 w-5"><circle cx="12" cy="12" r="10"></circle><path d="M12 16v-4"></path><path d="M12 8h.01"></path></svg>
                     News</h2>
-                    <div className="grid grid-cols-1 gap-4">
+                  <div className="grid grid-cols-1 gap-4">
+                    {animeNews && animeNews.map((item) => (
+                      <div key={mal_id}>
+                        <h2>{item.title}</h2>
+                      </div>
+                    ))}
 
-                    </div>
+                  </div>
                 </div>
               </div>
               <div data-state="inactive" data-orientation="horizontal" className={`w-full ${activeTab === 'episodes' ? '' : 'hidden'}`}>
