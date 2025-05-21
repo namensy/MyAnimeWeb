a26ebe232a736fd86c0a83896722cfdce5226736
namensy <Apiset1012@hotmail.com>
2025-05-21 17:13:48 +0700

    Add Prettier and Tailwind CSS plugins; update configuration files and styles

--- src/components/Allmovies.tsx
+++ src/components/Allmovies.tsx
@@ -1,77 +1,158 @@
-import { v4 as uuid } from 'uuid';
-import { useState } from "react";
-import { useAnimeApi } from "@/hooks/useAnimeApi";
-import { useAppContext } from '@/context/AppContext';
-import { formatSiUnit } from "format-si-unit"
-import Loading from '@/components/Loading';
-import { Link } from 'react-router-dom';
+import { v4 as uuid } from 'uuid'
+import { useState } from 'react'
+import { useAnimeApi } from '@/hooks/useAnimeApi'
+import { useAppContext } from '@/context/AppContext'
+import { formatSiUnit } from 'format-si-unit'
+import Loading from '@/components/Loading'
+import { Link } from 'react-router-dom'
 
 const Allmovies: React.FC = () => {
   const { searchTerm, loading } = useAppContext()
   const [currentIndex, setCurrentIndex] = useState(0)
-  const itemsPerPage = 6;
-  const cardWidth = 250; // px
+  const itemsPerPage = 6
+  const cardWidth = 250 // px
   const { animeList } = useAnimeApi(searchTerm)
 
   const handleRightClick = () => {
-    const maxIndex = Math.max(0, animeList.length - itemsPerPage);
+    const maxIndex = Math.max(0, animeList.length - itemsPerPage)
     if (currentIndex < maxIndex) {
-      setCurrentIndex(currentIndex + itemsPerPage);
+      setCurrentIndex(currentIndex + itemsPerPage)
     }
   }
 
   const handleLeftClick = () => {
     if (currentIndex - itemsPerPage >= 0) {
-      setCurrentIndex(currentIndex - itemsPerPage);
+      setCurrentIndex(currentIndex - itemsPerPage)
     } else {
-      setCurrentIndex(0);
+      setCurrentIndex(0)
     }
   }
 
-
   return (
-    <section className='w-full h-[600px] relativ mt-11 '>
-      <div className={` absolute left-0 bottom-82 md:bottom-51 lg:bottom-21 cursor-pointer z-10 md:py-28 lg:py-43 lg:px-3 bg-gradient-to-r from-[#00000088] from-100% to-transparent ${currentIndex === 0 ? 'hidden' : ''}`} onClick={handleLeftClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left size-8 text-neutral-300 cursor-pointer"><path d="m15 18-6-6 6-6"></path></svg>
+    <section className="w-full h-[600px] relativ mt-11 ">
+      <div
+        className={`absolute left-0 bottom-82 md:bottom-51 lg:bottom-21 cursor-pointer z-10 md:py-28 lg:py-43 lg:px-3 bg-gradient-to-r from-[#00000088] from-100% to-transparent ${currentIndex === 0 ? 'hidden' : ''}`}
+        onClick={handleLeftClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-chevron-left size-8 text-neutral-300 cursor-pointer"
+        >
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
       </div>
-      <div className={`absolute right-0 bottom-82 md:bottom-51 lg:bottom-21 cursor-pointer z-10 md:py-28 lg:py-43 lg:px-3 bg-gradient-to-r from-[#00000088] from-100% to-transparent ${currentIndex === 18 ? 'hidden' : ''}`} onClick={handleRightClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right size-8 text-neutral-300 cursor-pointer"><path d="m9 18 6-6-6-6"></path></svg>
+      <div
+        className={`absolute right-0 bottom-82 md:bottom-51 lg:bottom-21 cursor-pointer z-10 md:py-28 lg:py-43 lg:px-3 bg-gradient-to-r from-[#00000088] from-100% to-transparent ${currentIndex === 18 ? 'hidden' : ''}`}
+        onClick={handleRightClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-chevron-right size-8 text-neutral-300 cursor-pointer"
+        >
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
       </div>
       <div className="container w-11/12 mt-15 absolute left-4 sm:left-8 lg:left-12 xl:left-16 2xl:left-20 ">
-        <h1 className=" text-3xl font-bold tracking-wide">Anime Watchlist</h1>
-        <p className="text-[#9d9d9d]">Unwind this weekend with your top anime picks</p>
+        <h1 className=" text-3xl font-bold tracking-wide">
+          Anime Watchlist
+        </h1>
+        <p className="text-[#9d9d9d]">
+          Unwind this weekend with your top anime picks
+        </p>
         <div className="w-screen overflow-hidden relative">
           <div
             className="grid grid-flow-col auto-cols-[145px] md:auto-cols-[170px] lg:auto-cols-[200px] xl:auto-cols-[250px] gap-4 transition-transform duration-700 ease-in-out"
-            style={{ transform: `translateX(-${currentIndex * (cardWidth + 17)}px)` }}
+            style={{
+              transform: `translateX(-${currentIndex * (cardWidth + 17)}px)`,
+            }}
           >
             {loading ? (
               <Loading />
             ) : (
               animeList?.map((items) => (
-                <Link key={`${items.mal_id}-${uuid()}`} to={`/watch/${items.mal_id}`} className="w-full h-auto relative rounded-sm mt-5 ">
+                <Link
+                  key={`${items.mal_id}-${uuid()}`}
+                  to={`/watch/${items.mal_id}`}
+                  className="w-full h-auto relative rounded-sm mt-5 "
+                >
                   <div className="w-full aspect-[2/3] hover:bg-opacity-75 transition-all duration-300 ease-in-out">
-                    <img src={items.images.webp.image_url} alt="image" decoding='async' loading='lazy' className="block w-full h-full mx-auto rounded-sm object-cover" />
+                    <img
+                      src={items.images.webp.image_url}
+                      alt="image"
+                      decoding="async"
+                      loading="lazy"
+                      className="block w-full h-full mx-auto rounded-sm object-cover"
+                    />
                   </div>
-                  <p className="line-clamp-1 w-full text-white whitespace-normal mt-2 text-sm">{items.title}</p>
-                  <p className="text-[#9d9d9d] text-sm">{items.year ? items.year : 'No-info'}</p>
-                  <div className='absolute top-2 right-0 w-full h-full p-1 opacity-0 hover:opacity-95 hover:bg-[#17161c] transition-opacity duration-100 hover:scale-105'>
-                    <div className='whitespace-normal text-white text-sm line-clamp-10 md:line-clamp-8 lg:line-clamp-6 m-2 select-none'>
-                      <p className='mb-2 text-nowrap'>{items.title}</p>
-                      <p className='text-[#9d9d9d] '>{`${items.score} ★ (${formatSiUnit(items.scored_by)})`}</p>
-                      <p className='text-[#9d9d9d]'>{`${items.episodes} episodes`}</p>
-                      <p className='mt-2 line-clamp-4 '>{items.synopsis}</p>
+                  <p className="line-clamp-1 w-full text-white whitespace-normal mt-2 text-sm">
+                    {items.title}
+                  </p>
+                  <p className="text-[#9d9d9d] text-sm">
+                    {items.year ? items.year : 'No-info'}
+                  </p>
+                  <div className="absolute top-2 right-0 w-full h-full p-1 opacity-0 hover:opacity-95 hover:bg-[#17161c] transition-opacity duration-100 hover:scale-105">
+                    <div className="whitespace-normal text-white text-sm line-clamp-10 md:line-clamp-8 lg:line-clamp-6 m-2 select-none">
+                      <p className="mb-2 text-nowrap">
+                        {items.title}
+                      </p>
+                      <p className="text-[#9d9d9d] ">{`${items.score} ★ (${formatSiUnit(items.scored_by)})`}</p>
+                      <p className="text-[#9d9d9d]">{`${items.episodes} episodes`}</p>
+                      <p className="mt-2 line-clamp-4 ">
+                        {items.synopsis}
+                      </p>
                     </div>
-                    <div className='absolute bottom-4 left-4 flex gap-2 mb-4'>
+                    <div className="absolute bottom-4 left-4 flex gap-2 mb-4">
                       <div className="relative group">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-play mr-2 h-5 w-5 text-orange-500 text-xl"><polygon points="6 3 20 12 6 21 6 3"></polygon></svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-play mr-2 h-5 w-5 text-orange-500 text-xl"
+                        >
+                          <polygon points="6 3 20 12 6 21 6 3"></polygon>
+                        </svg>
                         <span className="absolute transform left-2 -translate-x-1/2 -translate-y-4/2 bg-[#535364] text-white text-sm lg:text-default rounded py-3 px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                           Play
                           <span className="absolute transform left-1/2 -translate-x-1/2 translate-y-7 lg:translate-y-6 bg-[#535364] w-2 h-2 lg:w-4 lg:h-4 rotate-45" />
                         </span>
                       </div>
                       <div className="relative group">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bookmark h-5 w-5 text-orange-500 text-xl"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-bookmark h-5 w-5 text-orange-500 text-xl"
+                        >
+                          <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path>
+                        </svg>
                         <span className="absolute transform left-1/2 -translate-x-1/2 -translate-y-4/2 bg-[#535364] text-white text-sm lg:text-default rounded py-3 px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-nowrap ">
                           Add to Watchlist
                           <span className="absolute transform left-1/2 -translate-x-1/2 translate-y-7 lg:translate-y-6 bg-[#535364] w-2 h-2 lg:w-4 lg:h-4 rotate-45" />
@@ -87,7 +168,7 @@ const Allmovies: React.FC = () => {
       </div>
       <div className="w-full h-full -mt-11 bg-black"></div>
     </section>
-  );
-};
+  )
+}
 
-export default Allmovies;
+export default Allmovies
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,134 +1,162 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import { backgrounds } from '@/assets/images'
-import { useMultipleAnimeApi } from '@/hooks/useMultipleAnimeApi';
+import { useMultipleAnimeApi } from '@/hooks/useMultipleAnimeApi'
 
 const Hero: React.FC = () => {
-  const [imageIndex, setImageIndex] = useState(1);
-  const [backgroundImage, setBackgroundImage] = useState<string>();
+    const [imageIndex, setImageIndex] = useState(1)
+    const [backgroundImage, setBackgroundImage] = useState<string>()
 
-  const { animes } = useMultipleAnimeApi([58567, 31240, 35507])
+    const { animes } = useMultipleAnimeApi([58567, 31240, 35507])
 
-
-  const getBackgroundImage = () => {
-    if (window.innerWidth < 768 && imageIndex === 1) {
-      return `${backgrounds.solo}`
-    } else if (window.innerWidth < 768 && imageIndex === 2) {
-      return `${backgrounds.rezeromobile}`
-    } else if (window.innerWidth > 768 && imageIndex === 1) {
-      return `${backgrounds.solomobile}`
-    } else if (window.innerWidth > 768 && imageIndex === 2) {
-      return `${backgrounds.rezero}`
-    } else if (imageIndex === 3) {
-      return `${backgrounds.cotemobile}`
+    const getBackgroundImage = () => {
+        if (window.innerWidth < 768 && imageIndex === 1) {
+            return `${backgrounds.solo}`
+        } else if (window.innerWidth < 768 && imageIndex === 2) {
+            return `${backgrounds.rezeromobile}`
+        } else if (window.innerWidth > 768 && imageIndex === 1) {
+            return `${backgrounds.solomobile}`
+        } else if (window.innerWidth > 768 && imageIndex === 2) {
+            return `${backgrounds.rezero}`
+        } else if (imageIndex === 3) {
+            return `${backgrounds.cotemobile}`
+        }
     }
-  };
 
-  const handleResize = () => {
-    setBackgroundImage(getBackgroundImage() ?? '')
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setImageIndex((prev) => (prev === 3 ? 1 : prev + 1));
-  //   }, 10000);
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  function extractAge(rating: string | undefined) {
-    if (!rating) return "N/A";
-    const match = rating.match(/(\d{1,2})\s*\+/)
-      || rating.match(/(\d{1,2})\s*or older/)
-      || rating.match(/(\d{1,2})\s*\+/)
-      || rating.match(/(\d{1,2})/)
-      ;
-    if (match) {
-      return `${match[1]}+`;
+    const handleResize = () => {
+        setBackgroundImage(getBackgroundImage() ?? '')
     }
-    if (rating.includes("All Ages")) return "ทุกวัย";
-    if (rating.includes("G")) return "ทุกวัย";
-    return "N/A";
-  }
 
-  const animeIdMap: Record<number, number> = {
-    1: 58567,
-    2: 31240,
-    3: 35507,
-  };
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
-  const animeBgMap: Record<number, string> = {
-    1: backgrounds.qwerty,
-    2: backgrounds.retext,
-    3: backgrounds.cote_text
-  }
+    // useEffect(() => {
+    //   const interval = setInterval(() => {
+    //     setImageIndex((prev) => (prev === 3 ? 1 : prev + 1));
+    //   }, 10000);
+    //   return () => clearInterval(interval);
+    // }, []);
 
-  const animesId = animeIdMap[imageIndex] || 35507;
-  const animesBg = animeBgMap[imageIndex];
+    function extractAge(rating: string | undefined) {
+        if (!rating) return 'N/A'
+        const match =
+            rating.match(/(\d{1,2})\s*\+/) ||
+            rating.match(/(\d{1,2})\s*or older/) ||
+            rating.match(/(\d{1,2})\s*\+/) ||
+            rating.match(/(\d{1,2})/)
+        if (match) {
+            return `${match[1]}+`
+        }
+        if (rating.includes('All Ages')) return 'ทุกวัย'
+        if (rating.includes('G')) return 'ทุกวัย'
+        return 'N/A'
+    }
 
-  return (
-    <main
-      className={`mx-auto w-4/4 lg:w-full h-full flex justify-center items-center transition-all ease duration-400 
+    const animeIdMap: Record<number, number> = {
+        1: 58567,
+        2: 31240,
+        3: 35507,
+    }
+
+    const animeBgMap: Record<number, string> = {
+        1: backgrounds.qwerty,
+        2: backgrounds.retext,
+        3: backgrounds.cote_text,
+    }
+
+    const animesId = animeIdMap[imageIndex] || 35507
+    const animesBg = animeBgMap[imageIndex]
+
+    return (
+        <main
+            className={`mx-auto w-4/4 lg:w-full h-full flex justify-center items-center transition-all ease duration-400 
         ${imageIndex === 1 && `bg-[left_20%_top]`} ${imageIndex === 3 && `bg-[left_45%_top]`} lg:bg-top bg-cover bg-no-repeat`}
-      style={{ backgroundImage: `url('${getBackgroundImage()}')`, }}
-    >
-      <div className="w-full h-full text-9xl double-gradient lg:pt-70">
-        <div className="container flex flex-col justify-center items-center md:items-start md:justify-start max-w-11/12 mx-auto">
-          <div className="w-4/7 md:w-3/7 lg:w-2/7 h-full mt-[330px] md:mt-20 lg:mt-0 mb-5 lg:mb-10 ">
-            <div className='h-[200px]'>
-              <img src={animesBg} alt={animesBg} decoding='async' loading='lazy' />
+            style={{ backgroundImage: `url('${getBackgroundImage()}')` }}
+        >
+            <div className="w-full h-full text-9xl double-gradient lg:pt-70">
+                <div className="container flex flex-col justify-center items-center md:items-start md:justify-start max-w-11/12 mx-auto">
+                    <div className="w-4/7 md:w-3/7 lg:w-2/7 h-full mt-[330px] md:mt-20 lg:mt-0 mb-5 lg:mb-10 ">
+                        <div className="h-[200px]">
+                            <img
+                                src={animesBg}
+                                alt={animesBg}
+                                decoding="async"
+                                loading="lazy"
+                            />
+                        </div>
+                    </div>
+                    <div className="w-full md:w-2/5 flex flex-col items-center justify-center md:items-start md:justify-start">
+                        <p className="text-sm text-[#9b9ba0] w-full h-full ">
+                            <span className="text-white inline-block px-[5px] bg-[#34373e] -skew-x-12 mb-3 lg:mb-5">
+                                {extractAge(animes?.[animesId]?.rating)}
+                            </span>
+                            {' • Sub | Dub • '}
+                            {animes?.[animesId]?.genres
+                                ?.map((genre) => genre.name)
+                                .join(' ,')}
+                        </p>
+                        <div className="hidden lg:block h-[97px]">
+                            <p className="text-base text-white text-justify line-clamp-4">
+                                {animes?.[animesId]?.synopsis}
+                            </p>
+                        </div>
+                        <div className="w-full min-w-[200px] md:max-w-[200px] md:w-2/5 text-black text-[16px] cursor-pointer py-1 pr-5 pl-2 mt-4 font-bold tracking-wide bg-[#ff640a] hover:bg-[#ff7b2e] transition-all">
+                            <Link
+                                to={`/watch/${animesId}`}
+                                className="w-full h-full flex justify-center items-center gap-1 cursor-pointer p-1"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="lucide lucide-play mr-2 h-5 w-5 text-black text-xl"
+                                >
+                                    <polygon points="6 3 20 12 6 21 6 3"></polygon>
+                                </svg>
+                                <p className="text-black text-sm">
+                                    START WATCHING
+                                </p>
+                            </Link>
+                        </div>
+                        <div className="flex gap-2 h-full items-end mt-8 lg:mt-15">
+                            <button
+                                type="button"
+                                onClick={() => setImageIndex(1)}
+                                className={`btn-style transition-all duration-400  ${
+                                    imageIndex === 1 ? 'bg-orange-400 w-12' : ''
+                                }`}
+                                aria-label="Show Solo Leveling"
+                            ></button>
+                            <button
+                                type="button"
+                                onClick={() => setImageIndex(2)}
+                                className={`btn-style transition-all duration-400  ${
+                                    imageIndex === 2 ? 'bg-orange-400 w-12' : ''
+                                }`}
+                                aria-label="Show Re:Zero"
+                            ></button>
+                            <button
+                                type="button"
+                                onClick={() => setImageIndex(3)}
+                                className={`btn-style transition-all duration-400  ${
+                                    imageIndex === 3 ? 'bg-orange-400 w-12' : ''
+                                }`}
+                                aria-label="Show Classroom of the Elite"
+                            ></button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="w-full md:w-2/5 flex flex-col items-center justify-center md:items-start md:justify-start">
-            <p className="text-sm text-[#9b9ba0] w-full h-full ">
-              <span className="text-white inline-block px-[5px] bg-[#34373e] -skew-x-12 mb-3 lg:mb-5">
-                {extractAge(animes?.[animesId]?.rating)}
-              </span>{" • Sub | Dub • "}
-              {animes?.[animesId]?.genres?.map(genre => (genre.name)).join(' ,')}
-            </p>
-            <div className="hidden lg:block h-[97px]">
-              <p className="text-base text-white text-justify line-clamp-4">
-                {animes?.[animesId]?.synopsis}
-              </p>
-            </div>
-            <div className='w-full min-w-[200px] md:max-w-[200px] md:w-2/5 text-black text-[16px] cursor-pointer py-1 pr-5 pl-2 mt-4 font-bold tracking-wide bg-[#ff640a] hover:bg-[#ff7b2e] transition-all'>
-              <Link to={`/watch/${animesId}`} className="w-full h-full flex justify-center items-center gap-1 cursor-pointer p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-play mr-2 h-5 w-5 text-black text-xl"><polygon points="6 3 20 12 6 21 6 3"></polygon></svg>
-                <p className='text-black text-sm'>START WATCHING</p>
-              </Link>
-            </div>
-            <div className="flex gap-2 h-full items-end mt-8 lg:mt-15">
-              <button
-                type='button'
-                onClick={() => setImageIndex(1)}
-                className={`btn-style transition-all duration-400  ${imageIndex === 1 ? "bg-orange-400 w-12" : ""
-                  }`}
-                aria-label="Show Solo Leveling"
-              ></button>
-              <button
-                type='button'
-                onClick={() => setImageIndex(2)}
-                className={`btn-style transition-all duration-400  ${imageIndex === 2 ? "bg-orange-400 w-12" : ""
-                  }`}
-                aria-label="Show Re:Zero"
-              ></button>
-              <button
-                type='button'
-                onClick={() => setImageIndex(3)}
-                className={`btn-style transition-all duration-400  ${imageIndex === 3 ? "bg-orange-400 w-12" : ""
-                  }`}
-                aria-label="Show Classroom of the Elite"
-              ></button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
+        </main>
+    )
+}
 
-export default Hero;
+export default Hero
