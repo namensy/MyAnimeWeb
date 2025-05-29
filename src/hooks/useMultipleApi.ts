089e4a32a6c9ae77728bed90ee15b397bf5f4045
namensy <Apiset1012@hotmail.com>
2025-05-29 17:58:54 +0700

    Refactor useMultipleApi hook to accept parameters for API calls and improve error handling

--- src/hooks/useMultipleApi.ts
+++ src/hooks/useMultipleApi.ts
@@ -1,4 +1,5 @@
 import { AnimeItems, Root } from '@/types'
+import { fail } from 'assert'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
@@ -24,10 +25,14 @@ const ResultAnimeResponse = (config: Config<AnimeResponse>) => {
   }
 }
 
-export const useMultipleApi = (api: Config) => {
+const controller = new AbortController()
+const signal = controller.signal
+
+export const useMultipleApi = (iknow: string, url: string) => {
   const [data, setData] = useState<AnimeItems[]>([])
   const [isLoading, setLoading] = useState<boolean>(false)
   const [isError, setIsError] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<String>('')
 
   // เราต้องการรับ Api ที่แตกต่างกันออกมา
   // ฉนั้นเราต้องรับ arg เป็นทั้ง Api แล้วก็ baseURL ของ API ก่อน
@@ -36,22 +41,32 @@ export const useMultipleApi = (api: Config) => {
   //ลองใช้งานดูก่อน
   const fetchApi = async () => {
     setLoading(true)
-    setIsError(false)
     try {
-      const endpoint = await axios.get(`${api.baseURL}${api.url}`)
+      const endpoint = await axios.get(`${iknow}${url}okman`, { signal })
       // เราสามารถทำ Pagination ได้
       const {
         data: { data },
       } = endpoint
       setData(data)
     } catch (error) {
-      console.log('This is error', error)
+      setIsError(true)
+      setLoading(false)
+      if (axios.isAxiosError(error)) {
+        setErrorMessage(error.response?.data || error.message)
+        console.log(error.message)
+        console.log(error.response?.data)
+      } else {
+        setErrorMessage('An unexpected error occured')
+        console.log('An unexpected error occured')
+      }
+    } finally {
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     fetchApi()
-  }, [])
+  }, [iknow, url])
 
   return {
     data,
